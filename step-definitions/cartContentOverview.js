2b1b4c4ee76c89a9d9145e185209f7713198b42e
Johan <siothaman@gmail.com>
2018-05-03 11:18:30 +0200

    Added and changed names for features & step definitions

--- step-definitions/cartContentOverview.js
+++ step-definitions/cartContentOverview.js
@@ -1,11 +1,11 @@
 // Require the parts of the program we want to test
-let Cart = require('../shopping-cart.js');  
+let Cart = require('../shopping-cart.js');
 
-// All our steps/tests should be wrapped in 
+// All our steps/tests should be wrapped in
 // moudule.exports = function(){ ... our code ...}
 module.exports = function(){
 
-  // Declaring variables I want to be able to share 
+  // Declaring variables I want to be able to share
   // between the different steps
   let name, age, aPerson;
 
@@ -33,3 +33,4 @@ this.Given(/^that we have a user$/, function (callback) {
          // Write code here that turns the phrase above into concrete actions
          callback(null, 'pending');
        });
+}
