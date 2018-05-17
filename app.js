27696932e14503986e2c59dc0bc2c27466f46c07
Urban <urban.eksvard@gmail.com>
2018-05-17 16:49:26 +0200

    fixed app for front- and back-end

--- app.js
+++ app.js
@@ -1,54 +1,61 @@
 // Read classes from other files
-let Person = require('./person.js');
-let Product = require('./product.js');
-let Category = require('./category.js');
+var Person = require("./person.js");
+var Product = require("./product.js");
+var Category = require("./category.js");
 
 class App {
+  constructor() {
+    let productData;
+    let categoryData;
 
-  constructor(){
-    // Read the JSON
-    let productData = require('./json/sortiment.json');
-    let categoryData = require('./json/categories.json');
+    if (typeof window !== "undefined") {
+      (async () => {
+        productData = await require("./json/sortiment.json");
+        categoryData = await require("./json/categories.json");
+        this.constructorContinued(productData, categoryData);
+      })();
+    } else {
+      productData = require("./json/sortiment.json");
+      categoryData = require("./json/categories.json");
+      this.constructorContinued(productData, categoryData);
+    }
+  }
 
+  constructorContinued(productData, categoryData) {
     // Make instances of Product from the productdata
     this.products = [];
-    for(let p of productData){
+    for (let p of productData) {
       this.products.push(new Product(p));
     }
 
     // Make instances of Category from categoryData
     this.categories = [];
-    for(let catName of categoryData){
+    for (let catName of categoryData) {
       this.categories.push(new Category(catName, this.products));
     }
 
     // Make a "dictionary" based on category namess
     this.categoryByName = {};
-    for(let category of this.categories){
+    for (let category of this.categories) {
       this.categoryByName[category.name] = category;
     }
 
     // Add a list of active/logged in user
     this.users = [];
-    
   }
 
-  addUser(name,birthDate){
-    this.users.push(new Person(name,birthDate));
+  addUser(name, birthDate) {
+    this.users.push(new Person(name, birthDate));
   }
-  
+
   //placeholder method
-  filter(filterString) {
-    
-  }
-
-  
+  filter(filterString) {}
 }
 
 // Create an app to start our application
 let myApp = new App();
 
-// Exporting the app instance so that I can use it 
+// Exporting the app instance so that I can use it
 // in my test code (step definitions) via require
 module.exports = myApp;
 
