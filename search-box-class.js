c903771ef3a63ae822f18ece32295c5955474cbd
Urban <urban.eksvard@gmail.com>
2018-05-17 16:38:14 +0200

    search function added

--- search-box-class.js
+++ search-box-class.js
@@ -1,13 +1,38 @@
+var assert = require("assert");
+var myApp = require("./app.js");
 module.exports = class SearchBox {
   //  constructor(){   }
 
   search(searchString) {
-    let searchList;
-    console.log(searchString);
-    return [];
+    let results = [];
+    if (
+      typeof searchString === "string" &&
+      /^[A-ZÅÄÖa-zåäö]$/.test(searchString)
+    ) {
+      results = myApp.products.filter(function(product) {
+        return (
+          product.namn.toString()[0] == searchString ||
+          product.namn2.toString()[0] == searchString
+        );
+      });
+    } else if (
+      typeof searchString === "string" ||
+      searchString instanceof String
+    ) {
+      results = myApp.products.filter(function(product) {
+        return (
+          product.namn.toString().includes(searchString) ||
+          product.namn2.toString().includes(searchString) ||
+          product.varugrupp.toString().includes(searchString) ||
+          product.ursprunglandnamn.toString().includes(searchString)
+        );
+      });
+    } else if (isNaN(searchString) == false) {
+      results = myApp.products.filter(function(product) {
+        return product.varnummer == searchString;
+      });
+    }
+
+    return results;
   }
-  //for (let i = 0; i < searchResult.length; i++) {
-  //        	if(str.search(SearchResult.[i])){
-  //        		return
-  //Object.create(searchResult)
 };
