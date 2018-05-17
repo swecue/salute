62c5611715932d487b1ead3a4cd661d613b2dc7b
Urban <urban.eksvard@gmail.com>
2018-05-17 16:49:04 +0200

    automatic prettify

--- person.js
+++ person.js
@@ -1,20 +1,16 @@
-let assert = require('assert');
-let ShoppingCart = require('./shopping-cart.js');
+var assert = require("assert");
+var ShoppingCart = require("./shopping-cart.js");
 
 module.exports = class Person {
-
-  constructor(name, birthDate){
-
+  constructor(name, birthDate) {
     // Throw an error if name is not an empty string
     assert(
-      typeof name === 'string' && name !== '',
-      'The name must be a non-empty string!'
+      typeof name === "string" && name !== "",
+      "The name must be a non-empty string!"
     );
 
     this.name = name;
     this.birthDate = birthDate;
     this.shoppingCart = new ShoppingCart();
-
   }
-
-}
+};
