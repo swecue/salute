0f6752dad29d103680d567b21f1689e9383802d7
Unknown <pourypm@gmail.com>
2018-05-31 12:14:03 +0330

    pull

--- features/sel-addToCart.feature
+++ features/sel-addToCart.feature
@@ -1,14 +1,14 @@
 Feature: As a user of Systembolagets online shop
 In order to buy beverages
-I want to be able  to add a certain quantity of a beverage to my shopping cart
+I want to be able  to add the beverages to my shopping cart
 
 
-Scenario: When user clicks the add button the website should 
+Scenario:user clicks the add button shopping cart overview should be updated  
     Given that I'm at the product page 
-    When I click the add product to cart button
-    Then the shopping cart overview should shows the added proudact  
+    When I click the add button product to cart 
+    Then the shopping cart overview should shows the added product 
 
-Scenario: When user filled the quantity-input box is whit number 
+Scenario:user filled the quantity-input box is whit number 
     Given that I'm at the product page 
     When I click  to fill the quantity-input box cart 
     Then the shopping cart overview should shows the added number 
--- step-definitions/sel-addToCart.js
+++ step-definitions/sel-addToCart.js
@@ -1,36 +1,61 @@
 let assert = require("assert");
 
 module.exports = function() {
-
-  // This does NOT give us all of jQuery's functions.
-  // It's just a shorter way of writing "findElem...."
-  function $(selector){
+    function $(selector) {
     return driver.findElement(by.css(selector));
-  }
+       }
 
+  
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+  this.Given(/^that I'm at the product page$/,async function() {
+    await helpers.loadPage("http://localhost:3000/product.html");
+    console.log("it works");    
+  });
 
+    this.When(/^I click the add button product to cart$/,async function() {
+  
+    let theButton = await $('#Add-btn');
 
-  this.Given(/^that I'm at the product page$/, async function () {
-         await helpers.loadPage('http://localhost:3000');
-         console.log("it works");
-         await sleep(1000);
+    assert(theButton, "The #Add-btn doesn't exist");
+    if (theButton) {
+      await theButton.click();
+      await sleep(1000);
+    }
+  });
+
+  
+        
+
+   this.Then(/^the shopping cart overview should shows the added product$/,async function  {
+         await helpers.loadPage("http://localhost:3000/mycart.html");
+         
        });
 
-
-     this.When(/^I click the add product to cart button$/,async function () {
-         let theButton = await driver.findElement(by.css(".Add-btn"));
-    await theButton.click();
-  });
+       this.When(/^I click  to fill the quantity\-input box cart$/,  {
+         async function() {
+      let userInput = "jord";
+      let field = await $("#sb-input-field");
+      assert(field, "The #sb-input-field doesn't exist");
+      if (field) {
+        console.warn("FIELD", field);
+        field.sendKeys(userInput);
+        await sleep(1000);
+      }
+    }
+  );
+  
+       
    
+       this.Then(/^the shopping cart overview should shows the added number$/, function (callback) {
+         // Write code here that turns the phrase above into concrete actions
+         callback(null, 'pending');
+       });
 
          
        
-this.Then(/^the shopping cart overview should shows the added proudact$/, async function () {
-         let
-       });
+};
 
 
- }
+ 
